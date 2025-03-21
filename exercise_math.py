9be280b750c64cc06f3e6e505f740e8db7b6d7f0
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2025-03-21 12:30:28 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,12 @@
 def math():
     a = 57
     b = 7
+    print (a+b)
+    print (a-b)
+    print (a*b)
+    avarage=(a+b)/2
+    print (avarage)
+    print(a//b)
+    print (a%b)
+    print (a/b)
+math()
